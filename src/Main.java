16cac938af0d76f6a2b5617b4879c540add53e5d
Stacy Degteva <degtich@mail.ru>
2022-01-03 02:59:26 +0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 1855;
+        int rub = 20;
+
+        float bonus = ticket / rub;
+        System.out.println(bonus);
+    }
+}
